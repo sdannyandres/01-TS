bdf8ecf96c2317a69d085cb66809315812e0291d
sdannyandres <sdannyandres@gmail.com>
2022-11-02 18:25:51 +0100

    1

--- app.ts
+++ app.ts
@@ -1,13 +1,28 @@
-interface Iint1{
-    a: number
-    b: string
-    c: Date
+interface Int0{
+    dato: number
 }
 
-var v1 = Iint1 = {
-    a: 1, 
-    b: "alberto", 
-    c: new Date()}
+interface Iint1{
+    a: number,
+    b: string,
+    c: Date,
+    h: Int0,
+}
+interface Iint2 extends Iint1{
+    d:number
+
+}
+var v1 : Iint2 = {
+    
+    a: 1,
+    b: "ALBERTO",
+    h:{dato:233},
+    c: new Date(),
+    d: 12
+   
+
+}
+
 
 
 
