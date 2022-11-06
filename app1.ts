83777b1be7a6a73c5b5d4c4260d3a48ad3ebdda0
sdannyandres <sdannyandres@gmail.com>
2022-11-06 13:57:02 +0100

    3

--- app1.ts
+++ app1.ts
@@ -1,16 +1,19 @@
 class Persona {
     private _nombre: string
-    constructor(nombre: string) {
+    private _saldo:number
+    constructor(nombre:string, saldo:number = 0) {
         this._nombre = nombre;
+        this._saldo = saldo;
     }
-    get nombre(): {
-        return this._nombre
+    get nombre(){ return this._nombre; }
+    get saldo(){ return this._saldo; }
+
+    addSaldo(importe:number):void {
+        this._saldo+= importe
+    }
+
 }
-var pepe = new Persona("pepe")
 
-console.log(pepe.nombre)
-
-var pepe = new Persona("Juan")
-
-console.log(pepe.nombre)
-
+var pepe = new Persona("pepe", 200)
+pepe.addSaldo(100)
+console.log(pepe.saldo)
